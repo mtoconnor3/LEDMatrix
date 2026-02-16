8766f9abeaa3e0aa5b23d48c9848dd5a7befa00f
mike <>
2026-02-16 16:23:12 +0100

    fiddle with bits

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from util import create_state_machines
 from matrix import LEDMatrix
 import constants
+import time
+import random
 
+def random_hex32_list():
+    return [random.getrandbits(32) for _ in range(8)]
+
+def bitwise_and(old, new):
+    return [a ^ b for a,b in zip(old, new)]
+
+# Fill the buffer with something
+oldbuffer = constants.TEST_PATTERN_ALL_ON
+
+# Set up the display
 data_sm, row_sm = create_state_machines()
 matrix = LEDMatrix(data_sm, row_sm)
-matrix.start(constants.TEST_PATTERN_32BIT)
+matrix.start(oldbuffer)
 
 # Both PIO state machines handle display timing autonomously.
 # Main loop just keeps the FIFOs fed and is otherwise free for application logic.
+
+# For periodically updating the buffer
+now = time.ticks_us()
 while True:
+    
+    # Refill the sm FIFO
     matrix.refill()
+
+    # Flip some bits occasionally
+    if time.ticks_diff(time.ticks_us(), now) >= 100000:
+        
+        newbuffer = bitwise_and(oldbuffer, random_hex32_list())
+        matrix.set_framebuffer(newbuffer)
+        oldbuffer = newbuffer
+        now = time.ticks_us()        
+        
